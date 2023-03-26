9766febac6bec4e1653dd66b224825ed912d45d2
Valentin Patrona <V.Patrona@peoplepower.ro>
2023-03-26 12:24:40 +0300

    find bombs in neighbour

--- minesweeper/src/app/services/board-state.service.ts
+++ minesweeper/src/app/services/board-state.service.ts
@@ -17,4 +17,53 @@ export class BoardStateService {
     [false,false,false,false,false,false,false,false,false,false],
     [false,false,false,false,false,false,false,false,false,false],
   ]
+
+
+  calculateNeighbour(x:number,y:number){
+    let neighbourBombs:number=0;
+
+    if(x!=0){
+      if(y!=0){
+        if(this.boardState[x-1][y-1])
+          neighbourBombs+=1
+      }
+
+      if(this.boardState[x-1][y])
+        neighbourBombs+=1
+
+      if(this.boardState[x-1][y+1])
+        neighbourBombs+=1
+
+    }
+
+    if(y<=this.boardState[0].length){
+      if(x<=this.boardState.length){
+        if(this.boardState[x+1][y+1])
+          neighbourBombs+=1
+      }
+      if(this.boardState[x][y+1])
+        neighbourBombs+=1
+
+    }
+
+    if(x<=this.boardState.length){
+      if(y>0){
+        if(this.boardState[x+1][y-1]){
+          neighbourBombs+=1;
+        }
+      }
+
+      if(this.boardState[x+1][y]){
+        neighbourBombs+=1
+      }
+    }
+
+    if(y>0){
+      if(this.boardState[x][y-1]){
+        neighbourBombs+=1;
+      }
+    }
+
+    return neighbourBombs
+  }
 }
